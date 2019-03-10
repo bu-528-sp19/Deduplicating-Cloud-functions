c8ea4c2289ed9537ec310bba875048aeb3377bcc
root <root@sanity.Shripad-Nadgowda-s-Account.cloud>
2019-03-10 16:44:40 +0000

    Bucket name change

--- python/kafkaConnect.py
+++ python/kafkaConnect.py
@@ -4,7 +4,7 @@ from kafka import KafkaConsumer
 
 def kafka_consumer(topic_name):
     consumer = KafkaConsumer(topic_name, auto_offset_reset='earliest',
-                             bootstrap_servers=['localhost:9092'], api_version=(0, 10), consumer_timeout_ms=1000)
+                             bootstrap_servers=['172.18.0.2:9092'], api_version=(0, 10), consumer_timeout_ms=1000)
 
     inf = 1
     while inf == 1:
@@ -17,4 +17,4 @@ def kafka_consumer(topic_name):
             consumer.close()
 
 if __name__ == "__main__":
-    kafka_consumer("test")
+    kafka_consumer("in-bucket-notifications")
