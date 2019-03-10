4229b8e4094f52ae1b35a7b03680f5bb9678bef0
paritoshshirodkar <paritoshshirodkar@gmail.com>
2019-03-10 01:43:30 -0500

    Consumer code to capture bucket events form kafka topic

--- bucket_notifications.py
+++ bucket_notifications.py
@@ -1,15 +1,15 @@
 from kafka import KafkaConsumer
-import json
+from json import loads
 
 consumer = KafkaConsumer(
     'in-bucket-notifications',
-    bootstrap_servers=['52.116.33.131:9092'],
-    auto_offset_reset='latest',
-    enable_auto_commit=True,
-    group_id='my-group',
-    api_version=(0,10))
+    bootstrap_servers=['172.18.0.2:9092'],
+    auto_offset_reset='latest')
     #value_deserializer=lambda x: loads(x.decode('utf-8')))
+    #enable_auto_commit=True,
+    #group_id='my-group',
+    #api_version=(0,10))
 
 for message in consumer:
-    record = json.loads(message.value)
+    record = loads(message.value)
     print(record)
