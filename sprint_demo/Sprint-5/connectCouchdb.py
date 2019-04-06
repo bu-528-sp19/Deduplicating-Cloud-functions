5845a6c0f77e2bec28a975354c5fe161024bd4c9
Vikash <vikashsahu4@gmail.com>
2019-04-06 13:52:18 -0400

    Sprint 5 code added

---
+++ sprint_demo/Sprint-5/connectCouchdb.py
@@ -0,0 +1,56 @@
+import couchdb
+
+def connect_couchdb():
+    couch = couchdb.Server("http://52.116.33.131:5984")
+    return couch
+
+def addFunctionIfNotExist(couch,functionChecksum,db_name="sanity"):
+
+    if db_name not in couch:
+        db = couch.create(db_name)
+
+        doc = {
+            functionChecksum: {
+            }
+        }
+        db.save(doc)
+    else:
+        db = couch[db_name]
+        for doc in db:
+            docs = db[doc]
+            if functionChecksum not in docs:
+                docs[functionChecksum]={}
+
+            db.save(docs)
+
+def addInputDataIfNotExist(couch,functionChecksum,inputChecksum,db_name="sanity"):
+    db = couch[db_name]
+    for id in db:
+        doc = db[id]
+        if inputChecksum not in doc[functionChecksum]:
+
+            doc[functionChecksum][inputChecksum]= ""
+            print(doc[functionChecksum][inputChecksum])
+            db.save(doc)
+
+def addMinioRef(couch,functionChecksum,inputChecksum,minioRef,db_name="sanity"):
+    db = couch[db_name]
+    for id in db:
+        doc = db[id]
+        if inputChecksum in doc[functionChecksum]:
+            doc[functionChecksum][inputChecksum]= minioRef
+            db.save(doc)
+        else:
+            print("The input checksum ",inputChecksum," not available")
+
+def verfiyDataAvailable(couch,functionChecksum,inputChecksum,db_name="sanity"):
+    db = couch[db_name]
+    for id in db:
+        doc = db[id]
+        if functionChecksum not in doc:
+            return None
+        if inputChecksum in doc[functionChecksum]:
+            return doc[functionChecksum][inputChecksum]
+        else:
+            return None
+
